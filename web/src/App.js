9885a4298c01103354cd3a028dd848552388fab5
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-01-18 21:35:59 -0300

    atualização de descrição básida do react

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // Componente: Bloco isolado de HTML, CSS e JSS, o qual não terfere  no restante da aplicação
 // Propriedade: Informações que um componente PAI passa para o componente FILHO
-// Estado: 
+// Estado: Informações mantidas pelos componentes (imutabilidade)
 
 function App() {
   return (<h1>Teste</h1>);
