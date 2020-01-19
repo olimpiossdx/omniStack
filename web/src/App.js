fadc63593897f40ee399231e22bfa8a61666631d
Olimpio Pimenta Barros <olimpiodx@gmail.com>
2020-01-18 21:32:04 -0300

    criando aplicação web e sua configuração inicial

---
+++ web/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// Componente: Bloco isolado de HTML, CSS e JSS, o qual não terfere  no restante da aplicação
+// Propriedade: Informações que um componente PAI passa para o componente FILHO
+// Estado: 
+
+function App() {
+  return (<h1>Teste</h1>);
+}
+
+export default App;
