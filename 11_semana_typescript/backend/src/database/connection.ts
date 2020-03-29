ac4219bd9232ed39baf164dbf57cd669ff3338d6
Olimpio <olimpiodx@gmail.com>
2020-03-29 17:29:35 -0300

    atualizando projetos sem typeScripit

--- 11_semana_typescript/backend/src/database/connection.ts
+++
@@ -1,8 +0,0 @@
-import knex from 'knex';
-import configuration from '../knexfile';
-
-const env = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development;
-
-const connection = knex(env);
-
-export default  connection;
